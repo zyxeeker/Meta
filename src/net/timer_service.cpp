76f00e896e93ef18d48d76ce35c85b99fed4df8c
zyxeeker <zyxeeker@gmail.com>
2022-05-19 03:50:32 +0000

    :memo: 增加从配置中读取客户端存活时间和容量

--- src/net/timer_service.cpp
+++ src/net/timer_service.cpp
@@ -1,9 +1,15 @@
 #include "timer_service.h"
 
+#include "component/config.h"
+
 namespace net {
 
 TimerService* TimerService::inst = nullptr;
 
-TimerService::TimerService() { m_lru.reset(new algo::timer::TimerLru(50, 50)); }
+TimerService::TimerService() {
+  m_lru.reset(new algo::timer::TimerLru(
+      com::Config::Instance()->config().client_alive_time,
+      com::Config::Instance()->config().client_capacity));
+}
 
 }  // namespace net
