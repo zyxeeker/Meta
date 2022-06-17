e28bb6d18e9c3e017b9acc423d481e41cc2f4e30
zyxeeker <zyxeeker@gmail.com>
2022-06-17 14:30:27 +0800

    :memo: 命名修改

--- src/component/config.h
+++ src/component/config.h
@@ -12,11 +12,11 @@ namespace com {
 
 class Config {
  public:
-  typedef struct url_redirect {
+  typedef struct reverse_proxy {
     std::string dst = "";           // 反代目标
     int port = 80;                  // 反代目标端口
     int allow_method = ALL_METHOD;  // 允许的方式
-  } UrlRedirectConfig;
+  } ReverseProxy;
 
   typedef struct config {
     std::string directory = "/home/www";  // 静态目录
@@ -25,8 +25,7 @@ class Config {
     u_int32_t client_alive_time = 180;    // 连接保活时长
     u_int32_t client_capacity = 50;       // 客户端容量
     u_int32_t read_buf_length = 4096;     // 读取缓存区大小
-    std::unordered_map<std::string, UrlRedirectConfig*>
-        redirect_settings;  // 反代字典
+    std::unordered_map<std::string, ReverseProxy*> proxy_settings;  // 反代字典
   } CFG;
 
   static Config* Instance() {
